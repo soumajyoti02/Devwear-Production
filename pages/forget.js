99a75236f565a86d48aa49c66b0c258aec2b97fb
Soumajyoti <soumyabwn3@gmail.com>
2023-04-24 20:24:19 +0530

    updated forget password email sending

--- pages/forget.js
+++ pages/forget.js
@@ -35,20 +35,6 @@ const Forget = () => {
 
     }, [])
 
-    // useEffect(() => {
-    //     const myuser = JSON.parse(localStorage.getItem('myuser'))
-    //     if (!myuser) {
-    //         router.push('/')
-    //     }
-
-    //     if (myuser && myuser.token) {
-    //         setUser(myuser)
-    //         setEmail(myuser.email)
-    //         fetchData(myuser.token)
-
-    //     }
-    // }, [])
-
     const sendResetEmail = async () => {
         let data = {
             email,
